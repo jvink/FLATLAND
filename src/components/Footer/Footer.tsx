75a972d9ef1e8d022f4f70535a7c441248ce9543
Jurian <0950140@hr.nl>
2021-10-26 19:52:35 +0200

    feat: 🎸 added footer and layout of page fixed!

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <h1>This is the footer!</h1>
+      </div>
+    </footer>
+  );
+}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 import styles from "./Layout.module.css";
 
 export default function Layout({ children }: { children: React.ReactElement }) {
   return (
-    <div className={styles["page-container"]}>
-      <Header />
-      {children}
-    </div>
+    <>
+      <div className={styles["page-container"]}>
+        <Header />
+        {children}
+      </div>
+      <Footer />
+    </>
   );
 }
